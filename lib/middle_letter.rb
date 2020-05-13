a362b03886e1d7c6849e8797299dda81ddb53fb1
sgneha <sgneha05@gmail.com>
2020-05-13 12:10:39 +0100

    test for single or double letter string

--- lib/middle_letter.rb
+++ lib/middle_letter.rb
@@ -1,3 +1,4 @@
-def get_middle(_str)
-  'x'
+def get_middle(str)
+  len = str.length
+  return str if len == 1 || len == 2
  end
--- spec/middle_letter_spec.rb
+++ spec/middle_letter_spec.rb
@@ -3,4 +3,7 @@ describe 'find middle letter' do
   it 'when single letter proivided' do
     expect(get_middle('x')).to eq 'x'
   end
+  it 'when string of two letters proivided' do
+    expect(get_middle('xy')).to eq 'xy'
+  end
 end
